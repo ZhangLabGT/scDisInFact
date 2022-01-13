c4ce2e8923bc9095836faf40d8986744e9c4a2b9
Xinye Zhao <xzhao429@gatech.edu>
2022-01-12 21:21:21 -0500

    update mmd

--- AntennaVAE/src/train.py
+++ AntennaVAE/src/train.py
@@ -2,30 +2,40 @@ import torch
 
 from loss_function import ZINB, maximum_mean_discrepancy
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# Training
-def train_epoch(encoder, decoder, output_layer, dataloader, optimizer, factor_zinb, factor_mmd):
+# Training    
+def train_epoch_mmd(encoder, decoder, output_layer, data_loader_1, data_loader_2, optimizer, factor_zinb, factor_mmd):
     encoder.train()
     decoder.train()
     output_layer.train()
     train_loss = 0.0
-    for sc_data_batch in dataloader:
-        sc_data_batch = sc_data_batch["count"].to(device)
+    for sc_data_batch in zip(data_loader_1, data_loader_2):
+        data_1, data_2 = sc_data_batch
+        data_1, data_2 = data_1['count'].to(device), data_2['count'].to(device)
+
         # Encode
-        encoded_data = encoder(sc_data_batch)
+        encoded_data_1 = encoder(data_1)
+        encoded_data_2 = encoder(data_2)
+
+        # Calculate MMD between latent spaces from two batches
+        mmd_loss = maximum_mean_discrepancy(encoded_data_1, encoded_data_2)
+        
         # Decode
-        decoded_data = decoder(encoded_data)
+        decoded_data_1 = decoder(encoded_data_1)
+        decoded_data_2 = decoder(encoded_data_2)
 
         # Compute params for DCA
-        mean_param, pi_param, theta_param = output_layer(decoded_data)
+        mean_param_1, pi_param_1, theta_param_1 = output_layer(decoded_data_1)
+        mean_param_2, pi_param_2, theta_param_2 = output_layer(decoded_data_2)
 
         # Evaluate loss
-        zinb = ZINB(pi_param, theta=theta_param, ridge_lambda=1e-5)
-        zinb_loss = (zinb.loss(mean_param, sc_data_batch))
+        zinb_1 = ZINB(pi_param_1, theta=theta_param_1, ridge_lambda=1e-5)
+        zinb_2 = ZINB(pi_param_2, theta=theta_param_2, ridge_lambda=1e-5)
 
-        # should be calculated between the latent space of two batches, not input and output
-        mmd_loss = maximum_mean_discrepancy(mean_param, sc_data_batch)
+        zinb_1_loss = (zinb_1.loss(mean_param_1, data_1))
+        zinb_2_loss = (zinb_2.loss(mean_param_2, data_2))
 
-        loss = zinb_loss * factor_zinb + mmd_loss * factor_mmd
+
+        loss = (zinb_1_loss + zinb_2_loss) * factor_zinb + mmd_loss * factor_mmd
         
         # Backword 
         optimizer.zero_grad()
@@ -33,8 +43,7 @@ def train_epoch(encoder, decoder, output_layer, dataloader, optimizer, factor_zi
         optimizer.step()
         
         train_loss += loss.item()
-    return train_loss / len(dataloader.dataset)
-
+    return train_loss / len(data_loader_1.dataset)
 # Testing func
 def test_epoch(encoder, decoder, output_layer, dataloader, factor_zinb, factor_mmd):
     encoder.eval()
--- AntennaVAE/src/utils.py
+++ AntennaVAE/src/utils.py
@@ -1,14 +1,61 @@
 import matplotlib.pyplot as plt
+import scanpy as sc
+from umap import UMAP
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+
+# Data preprocessing
+# Make sure the intput data is cell x gene format with gene on each column
+def preproc_filter(data1, data2, min_cells):
+    if not min_cells:
+        assert ValueError
+    cell_num1 = data1.shape[0]
+
+    print("Original shape of Data1:{} \t Data2:{}".format(data1.shape, data2.shape))
+
+    cmb_data = sc.concat([data1, data2])
+    sc.pp.filter_genes(cmb_data, min_cells=min_cells)
+
+    filtered_data1 = cmb_data[:cell_num1, :]
+    filtered_data2 = cmb_data[cell_num1:, :]
+    print('FIltered shape of Data1:{}, \t Data2: {}'.format(filtered_data1.shape, filtered_data2.shape))
+
+    return filtered_data1, filtered_data2
+
+def vis_latent_emb(data_Loader_1, data_loader_2, encoder, device):
+    rcParams["font.size"] = 20
+
+    umap_op = UMAP(n_components = 2, min_dist = 0.4, random_state = 0)
+    for data in data_Loader_1:
+        z_data_1 = encoder(data["count"].to(device))
+
+    for data in data_loader_2:
+        z_data_2 = encoder(data["count"].to(device))
+
+    z_umap_1 = umap_op.fit_transform(z_data_1.detach().cpu().numpy())
+    z_umap_2 = umap_op.fit_transform(z_data_2.detach().cpu().numpy())
+
+    # plot the figure
+    n_clust = 2
+    clust_color = plt.cm.get_cmap("Paired", n_clust)
+    fig = plt.figure(figsize = (10,7))
+    ax = fig.add_subplot()
+    ax.scatter(z_umap_1[:,0], z_umap_1[:, 1], color = clust_color(0), label = "Day4")
+    ax.scatter(z_umap_2[:,0], z_umap_2[:, 1], color = clust_color(1), label = "Day2")
+    ax.legend()
+    ax.set_xlabel("UMAP 1")
+    ax.set_ylabel("UMAP 2")
+
 # PLOT
-def plot_train(all_loss, seperate=False):
+def plot_train(all_loss, type=None):
     x_rg = range(len(all_loss['train_loss']))
     fig = plt.figure()
-    if not seperate:
+    if type == "sep":
         fig.add_subplot(1,1,1)
         plt.plot(x_rg, all_loss['train_loss'])
         plt.plot(x_rg, all_loss['test_loss'])
         plt.legend(['train_loss', 'test_loss'])
-    else:
+    elif type == 'cmb':
         ax1 = fig.add_subplot(2,2,1)
 
         ax1.plot(x_rg, all_loss['train_loss'])
@@ -24,10 +71,12 @@ def plot_train(all_loss, seperate=False):
         test_loss_fig = fig.add_subplot(2,2,3)
         test_loss_fig.plot(x_rg, all_loss['test_loss'])
         test_loss_fig.title.set_text('Test_Loss Fig')
+    elif type == None:
+        ax = fig.add_subplot(1,1,1)
+        
+        ax.plot(x_rg, all_loss['train_loss'])
+        ax.legend(['train_loss'])
+        ax.title.set_text('Train Loss Fig')
     plt.tight_layout()
     plt.show()
 
-    # Calculate Weight for loss
-    def weight_calculator(loss1, loss2):
-        factor = float.as_integer_ratio((loss1 / loss2))
-        return factor[0], factor[1]
