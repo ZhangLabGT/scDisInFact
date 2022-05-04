ac03298644e7314094bde39380b5e6b0160b3989
PeterZZQ <zhangziqibuaa@gmail.com>
2022-05-04 15:56:01 -0400

    add auprc, ari, and graph connectivity score

--- AntennaVAE/src/bmk.py
+++ AntennaVAE/src/bmk.py
@@ -14,6 +14,117 @@ from sklearn.decomposition import NMF
 import matplotlib.pyplot as plt
 
 
+
+def get_igraph_from_adjacency(adjacency, directed=None):
+    """Get igraph graph from adjacency matrix."""
+    import igraph as ig
+    sources, targets = adjacency.nonzero()
+    weights = adjacency[sources, targets]
+    if isinstance(weights, np.matrix):
+        weights = weights.A1
+    g = ig.Graph(directed=directed)
+    g.add_vertices(adjacency.shape[0])  # this adds adjacency.shape[0] vertices
+    g.add_edges(list(zip(sources, targets)))
+    try:
+        g.es['weight'] = weights
+    except:
+        pass
+    if g.vcount() != adjacency.shape[0]:
+        print( 'Your adjacency matrix contained redundant nodes.' )
+    return g
+
+
+def _compute_connectivities_umap(
+    knn_indices,
+    knn_dists,
+    n_neighbors,
+    set_op_mix_ratio=1.0,
+    local_connectivity=1.0,
+):
+    """\
+    This is from umap.fuzzy_simplicial_set [McInnes18]_.
+    Given a set of data X, a neighborhood size, and a measure of distance
+    compute the fuzzy simplicial set (here represented as a fuzzy graph in
+    the form of a sparse matrix) associated to the data. This is done by
+    locally approximating geodesic distance at each point, creating a fuzzy
+    simplicial set for each such point, and then combining all the local
+    fuzzy simplicial sets into a global one via a fuzzy union.
+    """
+
+    from umap.umap_ import fuzzy_simplicial_set
+    from scipy.sparse import coo_matrix
+
+    # place holder since we use precompute matrix
+    X = coo_matrix(([], ([], [])), shape=(knn_indices.shape[0], 1))
+    connectivities = fuzzy_simplicial_set(
+        X,
+        n_neighbors,
+        None,
+        None,
+        knn_indices=knn_indices,
+        knn_dists=knn_dists,
+        set_op_mix_ratio=set_op_mix_ratio,
+        local_connectivity=local_connectivity,
+    )
+
+    if isinstance(connectivities, tuple):
+        # In umap-learn 0.4, this returns (result, sigmas, rhos)
+        connectivities = connectivities[0]
+
+    return connectivities.tocsr()
+
+def leiden_cluster(
+    X = None, 
+    knn_indices = None,
+    knn_dists = None,
+    resolution = 30.0,
+    random_state = 0,
+    n_iterations: int = -1,
+    k_neighs = 30,
+    sigma = 1,
+    affin = None,
+    **partition_kwargs):
+
+    from sklearn.neighbors import NearestNeighbors
+
+    try:
+        import leidenalg
+    except ImportError:
+        raise ImportError(
+            'Please install the leiden algorithm: `conda install -c conda-forge leidenalg` or `pip3 install leidenalg`.'
+        )
+
+    partition_kwargs = dict(partition_kwargs)
+    
+    if affin is None:
+        if (knn_indices is None) or (knn_dists is None):
+            # X is needed
+            if X is None:
+                raise ValueError("`X' and `knn_indices & knn_dists', at least one need to be provided.")
+
+            neighbor = NearestNeighbors(n_neighbors = k_neighs)
+            neighbor.fit(X)
+            # get test connectivity result 0-1 adj_matrix, mode = 'connectivity' by default
+            knn_dists, knn_indices = neighbor.kneighbors(X, n_neighbors = k_neighs, return_distance = True)
+
+        affin = _compute_connectivities_umap(knn_indices = knn_indices, knn_dists = knn_dists, n_neighbors = k_neighs, set_op_mix_ratio=1.0, local_connectivity=1.0)
+        affin = affin.todense()
+        
+    partition_type = leidenalg.RBConfigurationVertexPartition
+    g = get_igraph_from_adjacency(affin, directed = True)
+
+    partition_kwargs['n_iterations'] = n_iterations
+    partition_kwargs['seed'] = random_state
+    partition_kwargs['resolution_parameter'] = resolution
+
+    partition_kwargs['weights'] = np.array(g.es['weight']).astype(np.float64)
+    part = leidenalg.find_partition(g, partition_type, **partition_kwargs)
+    groups = np.array(part.membership)
+
+    return groups
+
+
+
 #---------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------
 #
